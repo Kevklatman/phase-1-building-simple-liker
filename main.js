4ec4c58bf2ab9640b40099dd6236df5f2a52e1c3
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-07-31 16:50:57 -0600

    liker

--- main.js
+++ main.js
@@ -3,9 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const errorModal = document.getElementById('modal');
+  const errorMessage = document.getElementById('modal-message');
+  const hearts = document.querySelectorAll('.like-glyph');
 
-
-
+  hearts.forEach(heart => {
+    heart.addEventListener('click', () => {
+      if (heart.classList.contains('activated-heart')) {
+        heart.textContent = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
+      } else {
+        mimicServerCall()
+          .then(() => {
+            heart.textContent = FULL_HEART;
+            heart.classList.add('activated-heart');
+          })
+          .catch(error => {
+            errorMessage.textContent = error;
+            errorModal.classList.remove('hidden');
+            setTimeout(() => {
+              errorModal.classList.add('hidden');
+            }, 3000);
+          });
+      }
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
